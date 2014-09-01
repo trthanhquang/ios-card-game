44929f47b47d6d2d91d55cb343090da166e6d659
Louis <trthanhquang@gmail.com>
2014-08-31 17:36:14 -0700

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by LouisTran on 31/8/14.
+//  Copyright (c) 2014 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by LouisTran on 31/8/14.
+//  Copyright (c) 2014 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
